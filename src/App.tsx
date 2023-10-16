cfc6b005204915f28d7f0bfb3a4a60283e35eebc
piyawat pintusornsri <piyawat@softever.co.th>
2023-10-16 16:27:24 +0700

    test redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducers } from "./reducers";
+import * as someActions from "./actions/someAction";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
+  const someReducer = useSelector((state: RootReducers) => state.someReducer);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Stack spacing={6} direction="column">
+          <Button
+            onClick={() => dispatch(someActions.first() as any)}
+            variant="text"
+          >
+            Text
+          </Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
         <a
           className="App-link"
           href="https://reactjs.org"
