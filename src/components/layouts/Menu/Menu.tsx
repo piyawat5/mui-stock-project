4324e2ec3ab3a6a7fc6b41ad9be59d75d9e46c3f
piyawat pintusornsri <piyawat@softever.co.th>
2023-10-18 17:49:11 +0700

    setup menu in drawer.

--- src/components/layouts/Menu/Menu.tsx
+++ src/components/layouts/Menu/Menu.tsx
@@ -6,12 +6,16 @@ import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import { Link, useLocation } from "react-router-dom";
+import { Stack, SvgIconProps } from "@mui/material";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import PeopleIcon from "@mui/icons-material/People";
+import WidgetsIcon from "@mui/icons-material/Widgets";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import HouseIcon from "@mui/icons-material/House";
 
 const drawerWidth = 240;
 
@@ -29,8 +33,40 @@ type MenuProps = {
   open: boolean;
 };
 
+type UserMenu = {
+  name: string;
+  link: string;
+  icon: React.ReactElement<SvgIconProps>;
+}[];
+
+type AdminMenu = {
+  name: string;
+  link: string;
+  icon: React.ReactElement<SvgIconProps>;
+}[];
+
 export default function Menu({ open, onDrawerClose }: MenuProps) {
   const theme = useTheme();
+  const userMenu: UserMenu = [
+    {
+      name: "Home",
+      link: "/home",
+      icon: <HouseIcon></HouseIcon>,
+    },
+    {
+      name: "Shopping",
+      link: "/shopping",
+      icon: <StorefrontIcon></StorefrontIcon>,
+    },
+    { name: "About us", link: "/about-us", icon: <PeopleIcon></PeopleIcon> },
+  ];
+  const adminMenu: AdminMenu = [
+    { name: "Stock", link: "/stock", icon: <WidgetsIcon></WidgetsIcon> },
+    { name: "Report", link: "/report", icon: <BarChartIcon></BarChartIcon> },
+  ];
+
+  const location = useLocation();
+  const path = location.pathname;
 
   return (
     <Drawer
@@ -47,42 +83,53 @@ export default function Menu({ open, onDrawerClose }: MenuProps) {
       open={open}
     >
       <DrawerHeader>
-        <IconButton
-          onClick={() => {
-            onDrawerClose();
-          }}
-        >
-          {theme.direction === "ltr" ? (
-            <ChevronLeftIcon />
-          ) : (
-            <ChevronRightIcon />
-          )}
-        </IconButton>
+        <Stack direction="row" justifyContent={"center"} alignItems={"center"}>
+          {/* TODO: Add Logo*/}
+          {/* <img 
+          src={`${process.env.PUBLIC_URL}/images/cm_logo.png`}
+          style={{height: 30}}
+          /> */}
+          <IconButton
+            onClick={() => {
+              onDrawerClose();
+            }}
+          >
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </Stack>
       </DrawerHeader>
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
+        {userMenu.map((menu, index) => (
+          <ListItemButton
+            sx={{ my: 1, pl: 5 }}
+            key={menu.name}
+            selected={menu.link === path}
+            to={menu.link}
+            component={Link}
+          >
+            <ListItemIcon>{menu.icon}</ListItemIcon>
+            <ListItemText primary={menu.name} />
+          </ListItemButton>
         ))}
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
+        {adminMenu.map((menu, index) => (
+          <ListItemButton
+            sx={{ my: 1, pl: 5 }}
+            key={menu.name}
+            selected={menu.link === path}
+            to={menu.link}
+            component={Link}
+          >
+            <ListItemIcon>{menu.icon}</ListItemIcon>
+            <ListItemText primary={menu.name} />
+          </ListItemButton>
         ))}
       </List>
     </Drawer>
