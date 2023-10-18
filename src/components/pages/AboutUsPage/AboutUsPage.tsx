f186b642e4417afc09abec1ae7f266edba84959d
piyawat pintusornsri <piyawat@softever.co.th>
2023-10-18 13:04:15 +0700

    create aboutUs and productDetailPage

---
+++ src/components/pages/AboutUsPage/AboutUsPage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+type AboutUsPageProps = {
+  //
+};
+
+const AboutUsPage: React.FC<any> = () => {
+  return <div>AboutUsPage</div>;
+};
+
+export default AboutUsPage;
---
+++ src/components/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+type ProductDetailPageProps = {
+  //
+};
+
+const ProductDetailPage: React.FC<any> = () => {
+  return <div>ProductDetailPage</div>;
+};
+
+export default ProductDetailPage;
