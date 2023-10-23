bb4e873a0898273408388eb8563aa7245e3343d6
piyawat pintusornsri <piyawat5930302600@gmail.com>
2023-10-23 10:29:02 +0700

    fix Login page

--- src/components/pages/LoginPage/LoginPage.tsx
+++ src/components/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import { Formik, FormikProps } from "formik";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Stack,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { CSSProperties, useState } from "react";
+import MyDatepicker from "../../features/MyDatepicker";
 
-type LoginPageProps = {
-  //
-};
+// type LoginProps = {
+
+// };
 
 type Account = {
-  username?: string;
-  password?: string;
+  username: string;
+  password: string;
 };
 
 const LoginPage: React.FC<any> = () => {
@@ -18,75 +29,78 @@ const LoginPage: React.FC<any> = () => {
     username: "",
     password: "",
   };
+  const classes: { [key: string]: CSSProperties } = {
+    root: { display: "flex", justifyContent: "center" },
+    card: { padding: "8px", width: 300 },
+    buttons: { marginTop: 2 },
+  };
 
   const Form = ({
     handleSubmit,
     handleChange,
+    setFieldValue,
     isSubmitting,
     values,
   }: FormikProps<Account>) => {
     return (
-      <form
-        action=""
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: 24,
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onSubmit={handleSubmit}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          <label htmlFor="username">username</label>
-          <input
+      <form action="" onSubmit={handleSubmit}>
+        <Stack direction="column" spacing={4}>
+          <TextField
             onChange={handleChange}
             value={values.username}
-            type="text"
-            placeholder="Username"
-            name="username"
             id="username"
+            label="Username"
+            variant="outlined"
+            autoFocus
             required
-          />
-        </div>
-
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-          }}
-        >
-          <label htmlFor="password">password</label>
-          <input
+            fullWidth
+          ></TextField>
+          <TextField
             onChange={handleChange}
             value={values.password}
-            type="text"
-            placeholder="Password"
-            name="password"
             id="password"
+            label="password"
+            type="password"
+            variant="outlined"
+            fullWidth
             required
-          />
-        </div>
-        <button disabled={isSubmitting} style={{ width: "100%" }} type="submit">
-          SUBMIT
-        </button>
+          ></TextField>
+          <Stack direction={"column-reverse"} spacing={2}>
+            <Button
+              variant="outlined"
+              disabled={isSubmitting}
+              color="primary"
+              type="button"
+              fullWidth
+            >
+              CANCEL
+            </Button>
+            <Button
+              sx={{
+                color: "#fff",
+              }}
+              variant="contained"
+              disabled={isSubmitting}
+              color="primary"
+              type="submit"
+              fullWidth
+            >
+              SUBMIT
+            </Button>
+          </Stack>
+        </Stack>
       </form>
     );
   };
 
   return (
     <>
-      <Box>
-        <Card>
+      <Box sx={classes.root}>
+        <Card sx={classes.card}>
           <CardContent>
-            <Typography>Login</Typography>
+            <Typography fontSize={20} marginBottom={4}>
+              Login
+            </Typography>
             <Formik
               onSubmit={(value, { setSubmitting }) => {
                 alert(JSON.stringify(value));
